66cad9b5b5b3b173e19d9858f5c5a169a49df2ba
Ezekwe chibuikem <nwachinaemelum@yahoo.com>
2024-11-11 10:38:43 +0100

    task 0 fixing

--- 0x04-TypeScript/task_0/.eslintrc.js
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -1,12 +1,28 @@
-module.exports =  {
-  parser:  '@typescript-eslint/parser',
-  extends:  [
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+      'plugin:@typescript-eslint/recommended',
   ],
-  parserOptions:  {
-    ecmaVersion:  2018,
-    sourceType:  'module',
+  plugins: ['@typescript-eslint'],
+  env: {
+      browser: true,
+      es6: true,
+      node: true,
   },
-  rules:  {
+  rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
   },
 };
+
+// module.exports =  {
+//   parser:  '@typescript-eslint/parser',
+//   extends:  [
+//     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+//   ],
+//   parserOptions:  {
+//     ecmaVersion:  2018,
+//     sourceType:  'module',
+//   },
+//   rules:  {
+//   },
+// };
--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,37 +1,72 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: "./js/main.ts",
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  devServer: {
-    contentBase: "./dist"
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Development"
-    })
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
+    mode: 'development',
+    entry: './js/main.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
+
+// const path = require("path");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+// module.exports = {
+//   entry: "./js/main.ts",
+//   devtool: "inline-source-map",
+//   module: {
+//     rules: [
+//       {
+//         test: /\.tsx?$/,
+//         loader: 'ts-loader',
+//         options: {
+//           transpileOnly: true
+//         }
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: [".tsx", ".ts", ".js"]
+//   },
+//   devServer: {
+//     contentBase: "./dist"
+//   },
+//   plugins: [
+//     new ForkTsCheckerWebpackPlugin(),
+//     new CleanWebpackPlugin(),
+//     new HtmlWebpackPlugin({
+//       title: "Development"
+//     })
+//   ],
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname, "dist")
+//   }
+// };
